55f10612625009efa642245aeb089609435dc667
Catherine Lesnova <arch.lesnova@gmail.com>
2018-10-14 21:19:04 +0500

    responsive all

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $('.slider').slick({
     arrows: false,
     responsive: [
       {
-        breakpoint: 900,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
@@ -24,22 +24,19 @@ $('.slider').slick({
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 900,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 400,
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
           
