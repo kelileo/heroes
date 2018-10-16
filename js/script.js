aa20a82fa7939eed63a15b6ae2caa5784f72a59e
Catherine Lesnova <arch.lesnova@gmail.com>
2018-10-16 16:55:14 +0500

    ready to test

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 'use strict';
 
 validateForm();
-  
+
 function checkInput(input) {  
     var value = input.value;
     if (input.dataset.hasOwnProperty('required') && !value) {
@@ -14,14 +14,16 @@ function checkInput(input) {
 function validateForm() {
     var form = document.getElementById('add-hero');
     var inputs = Array.from(
-        document.querySelectorAll('#add-hero' + ' input')
+        document.querySelectorAll('.text-input')
     ); 
-
+    var loader = document.querySelector('.loader');  
+    var loaderBtn = loader.parentNode;
+    
     form.addEventListener('focus', function (event) {
         var target = event.target;
         if (target.tagName === 'INPUT') {
             target.classList.remove('input_error');
-        }
+        }      
     }, true);
 
     form.addEventListener('blur', function (event) {
@@ -30,9 +32,13 @@ function validateForm() {
             if (!checkInput(target)) {
                 target.classList.add('input_error');
             }
-        }
-    }, true);
-     
+        }        
+    }, true); 
+
+    loaderBtn.addEventListener('click', function() {
+        loaderBtn.classList.remove('input_error'); 
+    }); 
+          
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         form.classList.remove('form_valid');
@@ -43,15 +49,29 @@ function validateForm() {
             var input = inputs[i];
   
             if (!checkInput(input)) {
-                input.classList.add('input_error');
+                input.classList.add('input_error'); 
                 hasError = true;
             }
+        }
+        if (!checkInput(loader)) {
+            loaderBtn.classList.add('input_error'); 
+            hasError = true;
         }
   
         if (hasError) {
             form.classList.add('form_invalid');
         } else {
             form.classList.add('form_valid');
-        }
+        }        
     });
 }
+
+//вывод названия загруженного файлы
+$(document).ready( function() {
+    $(".file-upload input[type=file]").change(function() {
+         var filename = $(this).val().replace(/.*\\/, "");
+         $("#filename").val(filename);
+         $("#placeholder").html('Чтобы добавить другое фото, перетащите изображение в это поле или кликните сюда');
+    });
+});
+
